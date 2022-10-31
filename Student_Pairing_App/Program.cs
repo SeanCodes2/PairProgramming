25288088314eddde81ae301975310248e38b25b2
SeanCodes2 <sean.coyan@outlook.com>
2022-10-31 14:57:55 -0500

    InitialPush

--- Student_Pairing_App/Program.cs
+++ Student_Pairing_App/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int students = 0;
+            int numGroups = 0;
+
+            Console.Write("How many students are there?");
+            students = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            Console.WriteLine(students);
         }
     }
 }
---
+++ Student_Pairing_App/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Pairing_App
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public Student()
+        {
+
+        }
+    }
+}
