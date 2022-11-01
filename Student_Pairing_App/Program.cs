6b53f7e8a9f0c7291a4ac275bc0a5bcc7ffc0e93
jpwalls08 <jpwalls08@outlook.com>
2022-11-01 09:19:14 -0500

    Pair Programming phase 1

--- Student_Pairing_App/Program.cs
+++ Student_Pairing_App/Program.cs
@@ -4,15 +4,58 @@
     {
         static void Main(string[] args)
         {
-            int students = 0;
-            int numGroups = 0;
+            //creeting a list
+            //do-while loop for names
+            //
+            List<string> names = new List<string> ();
 
-            Console.Write("How many students are there?");
-            students = Convert.ToInt32(Console.ReadLine());
+            bool name = true; 
 
+            do
+            {
+
+                Console.WriteLine("Please enter your name");
+                string userInput = Console.ReadLine();
+
+                names.Add(userInput); //add to the list above
+
+                Console.WriteLine("Press Esc or Y to add another name");
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.Escape)
+                {
+                    name = false;
+                }
+
+            } while (name);
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Divide into two groups
+            //Iterate through two groups
+            //Random numbers
+
+            while (names.Count() > 0)
+            {
+                if (names.Count() % 2 == 1)
+                {
+                    Console.WriteLine($"{names[0]}, {names[1]}, {names[2]}"); 
+
+                    names.RemoveRange(0, 3); // kicks the names out the list
+                }
+
+                else
+                {
+                    Console.WriteLine($"{names[0]}, {names[1]} "); //names divided into list
+
+                    names.RemoveRange(0, 2);
+                }
+            }
             
-
-            Console.WriteLine(students);
         }
     }
 }
