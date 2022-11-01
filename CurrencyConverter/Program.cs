e702064fd484fd14020b0e51381ee379f88bd9d2
SeanCodes2 <sean.coyan@outlook.com>
2022-11-01 09:59:00 -0500

    Started MoneyManager

---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+             * All users will start with a balance of $5000.
+                Users should have an account balance for all of the following types of currency:
+                - Cash
+                - Bitcoin
+                - Etherium
+                - Litecoin
+                User should have the option to do any of the following actions:
+                - Purchase digital currency using cash.
+                - Sell digital currency for cash.
+                - View all (4) account balances.
+                - Transfer between digital currency types.
+                Assume the following values:
+                - Each Bitcoin is worth $7480
+                - Each Etherium is worth $586.15   5(balnce 5xrate) cash value 
+                - Each Litecoin is worth $119.04
+             * 
+             * Planning:
+             * 
+             * Classes:
+             * 
+             *     
+             * Cash 
+             *      conversion rate
+             *           
+             * Bitcoin 
+             *       conversion rate
+             *       balance in cash
+             * Etherium.rate
+             *       conversion rate
+             *       balance in cash
+             * Litecoin 
+             *       conversion rate
+             *       balance balance
+             *           
+             * MoneyManager UI       
+             *           
+             *           
+             * Methods:
+             *      
+             * conversion(currency, currency) {
+             *      newBalance = currency1.balnce * currency2.rate
+             *      return newBalance
+             * }
+             * 
+             */
+        }
+    }
+}
